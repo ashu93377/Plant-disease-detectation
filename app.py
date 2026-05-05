37997f999d6f38ee9ea8814fe8f3a1642afadea2
Your Name <your-email@example.com>
2026-05-05 12:19:03 +0530

    fix render port binding

--- app.py
+++ app.py
@@ -84,5 +84,9 @@ def market():
     return render_template('market.html', supplement_image = list(supplement_info['supplement image']),
                            supplement_name = list(supplement_info['supplement name']), disease = list(disease_info['disease_name']), buy = list(supplement_info['buy link']))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
